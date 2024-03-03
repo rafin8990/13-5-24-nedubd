4cc639b89188fa85b66a509c2fd5369378fea69b
AsmaCSE <asmaakter2331@gmail.com>
2024-03-03 12:57:04 +0600

    side bar

--- resources/views/Backend/AdmitCard/Report(admitCards)/listAdminInstruction.blade.php
+++ resources/views/Backend/AdmitCard/Report(admitCards)/listAdminInstruction.blade.php
@@ -11,7 +11,7 @@ Instruction List
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-10 md:my-10">
         <div class="grid gap-6 mb-6 md:grid-cols-4 ">
             <button type="button"
-                class=" md:mr-20 text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add New
+                class=" md:mr-20 text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><a href="{{url('/dashboard/AddAdmitInstruction')}}">Add New</a>
                 </button>
 
            
