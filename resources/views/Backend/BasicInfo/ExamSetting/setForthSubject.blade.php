5041e67ca9c0c8ded672063ca1706f00097a6224
rafin8990 <rafin8990@gmail.com>
2024-03-04 17:51:30 +0600

    code added

--- resources/views/Backend/BasicInfo/ExamSetting/setForthSubject.blade.php
+++ resources/views/Backend/BasicInfo/ExamSetting/setForthSubject.blade.php
@@ -3,6 +3,9 @@
 4th Subject Setup
 @endsection
 @section('Dashboard')
+
+@include('Message.message')
+
 <div>
     <h3>
         Fourth Subject Setup
@@ -241,14 +244,17 @@
     <br><br>
     <div class="md:flex justify-center">
         <div class="mr-10">
-            <button type="submit" class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View And Delete</button>
+            <a href="/dashboard/viewFourthSubject">
+            <p class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View And Delete</p>
+            </a>
+            
         </div>
         <div class="mr-10">
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
         </div>
         <div class="mr-10">
             <a href="/dashboard">
-            <button type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Close</button>
+            <button  class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Close</button>
             </a>
         </div>
 
