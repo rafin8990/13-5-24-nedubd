18692cc808143f0d339dc5c6d015edee2a8d1b97
Sayemsaadat0 <sayemsaadato@gmail.com>
2024-03-09 18:08:48 +0600

    report name getting data

--- resources/views/Backend/BasicInfo/ExamSetting/setSignature.blade.php
+++ resources/views/Backend/BasicInfo/ExamSetting/setSignature.blade.php
@@ -19,11 +19,12 @@
                     <select id="countries" name="report_name"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option selected>Choose Report</option>
-                        <option value="">x</option>
-                        <option value="">y</option>
-                        <option value="">z</option>
+                        @foreach($reports as $report)
+                        <option >{{$report->report_name}}</option>
+                        @endforeach
                     </select>
                 </div>
+
 
                 <div>
                     <button type="submit"
