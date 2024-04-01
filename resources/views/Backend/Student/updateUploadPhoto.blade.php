3804b6daca495cbcc2f8cce96a682d55fb97938b
rafin8990 <rafin8990@gmail.com>
2024-04-01 14:59:02 +0600

    code added

--- resources/views/Backend/Student/updateUploadPhoto.blade.php
+++ resources/views/Backend/Student/updateUploadPhoto.blade.php
@@ -105,11 +105,11 @@
 
                 
     
-              <div>
+              <!-- <div>
               <input type="text"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Search student " />
-              </div>
+              </div> -->
               <div>
                     <input class="hidden" name="school_code" value="{{$school_code}}" type="text">
                 </div>
