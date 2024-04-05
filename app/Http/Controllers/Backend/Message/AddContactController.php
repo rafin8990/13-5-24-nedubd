6d531995526b48ee06cb8026ec9bd1f0e3a0cf17
rafin8990 <rafin8990@gmail.com>
2024-04-05 15:56:44 +0600

    send sms done

--- app/Http/Controllers/Backend/Message/AddContactController.php
+++ app/Http/Controllers/Backend/Message/AddContactController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers\Backend\Message;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class AddContactController extends Controller
 {
    public function Contact(){
     return view ('Backend.Messaging.addContact');
+   }
+
+   public function addContact(Request $request){
+      $existingContact=Contact::where('school_code',$request->school_code)->where('action','approved')->where('contact',$request->contact);
+
+      if($existingContact){
+         return redirect()->back()->with('error','Contact already added');
+      }
+         $contact=new Contact();
+         $contact->name = $request->name;
+         $contact->contact = $request->contact;
+         $contact->school_code = $request->school_code;
+         $contact->action = "approved";
+         $contact->save();
+
+         return redirect()->back()->with('success','Contact added successfully');
+
    }
    
 }
--- resources/views/Backend/Messaging/sendMessage.blade.php
+++ resources/views/Backend/Messaging/sendMessage.blade.php
@@ -14,19 +14,28 @@
         <h1 class="text-2xl font-bold my-10 mx-5 text-center">Send Message</h1>
     </div>
     <div class=" mb-3">
-        <form action="" class="p-5 shadowStyle rounded-[8px] border border-slate-300 w-3/5 mx-auto space-y-3">
+        <form action="{{route('sendMessage')}}" method="POST" class="p-5 shadowStyle rounded-[8px] border border-slate-300 w-3/5 mx-auto space-y-3">
+        @csrf
             <div class="grid grid-cols-3 place-items-start  gap-5">
                 <label for="class" class="block mb-2 text-sm font-medium whitespace-noWrap ">Instruction
                     :</label>
-                    <textarea name="instruction" id="instruction" rows="4"
+                    <textarea name="message" id="instruction" rows="4"
                         class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
                         placeholder="write a message"></textarea>
+                        <input value="{{$school_code}}" name="school_code" class="hidden" type="text">
+            </div>
+
+            <div class="w-full flex justify-end">
+                <button type="submit"
+                    class="  text-white bg-rose-700 hover:bg-rose-600 focus:ring-0  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Send
+                </button>
             </div>
             <div class="">
                 <h3 class="text-lg mb-5">
                     All Contact:
                 </h3>
-                <table class="w-full text-sm rtl:text-right text-black">
+
+                <table class="w-full text-sm text-center rtl:text-right text-black">
                     <thead class="text-center text-white uppercase bg-blue-300 border-b border-blue-200 ">
                         <tr>
                             <th scope="col" class=" text-sm px-6 py-3 bg-blue-500">
@@ -35,20 +44,33 @@
                             <th scope="col" class=" text-sm px-6 py-3">
                                 Contact
                             </th>
+                            <th scope="col" class=" text-sm px-6 py-3 bg-blue-500">
+                                Status
+                            </th>
                             
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach($contacts as $data)
+                        <tr>
+                            <td scope="col" class=" text-sm px-6 py-3">
+                                {{$data->name}}
+                            </td>
+                            <td scope="col" class=" text-sm px-6 py-3">
+                                {{$data->contact}}
+                            </td>
+                            <td scope="col" class=" text-sm px-6 py-3">
+                                <input type="checkbox" name="contact[{{$data->contact}}]" value="{{$data->contact}}" checked id="">
+                            </td>
+                            
+                        </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
             
             
-            <div class="w-full flex justify-end">
-                <button type="submit"
-                    class="  text-white bg-rose-700 hover:bg-rose-600 focus:ring-0  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Send
-                </button>
-            </div>
+            
         </form>
     </div>
 @endsection
